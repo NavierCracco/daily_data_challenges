19f29b69cb38ad421c549cb93031113a305a2ca5
Navier Cracco <cracconavier@gmail.com>
2025-12-30 17:34:00 -0300

    Day 5: added SQL(1683) solution

---
+++ SQL/05_invalid_tweets.sql
@@ -0,0 +1,15 @@
+/*
+Plataforma: LeetCode
+Problema: 1683. Invalid Tweets
+Dificultad: Easy
+
+Descripción:
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+*/
+
+SELECT
+    tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15;
