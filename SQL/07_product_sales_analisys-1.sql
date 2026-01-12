c6dcd381ce322e3e29574edae8a785006b3d996b
Navier Cracco <cracconavier@gmail.com>
2026-01-12 18:22:27 -0300

    Day 7: added SQL(1068) solution

---
+++ SQL/07_product_sales_analisys-1.sql
@@ -0,0 +1,27 @@
+/*
+Plataforma: LeetCode
+Problema: 1068. Product Sales Analysis I
+Dificultad: Easy
+
+Descripción:
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+
+
+Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+Return the resulting table in any order.
+*/
+
+SELECT 
+    p.product_name,
+
+    s.year,
+    s.price
+
+FROM Sales AS s
+LEFT JOIN Product AS p ON s.product_id = p.product_id;
